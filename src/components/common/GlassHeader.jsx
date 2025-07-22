81ed56ec7c074c8065a12ce7d5fa37088927f704
brian0309 <34000638+brian0309@users.noreply.github.com>
2025-07-22 21:47:15 +0800

    Update footer, update header styling

--- src/components/common/GlassHeader.jsx
+++ src/components/common/GlassHeader.jsx
@@ -16,7 +16,7 @@ const GlassHeader = ({ children }) => {
   return (
     <div className="w-full">
       <header 
-        className={`m-0 px-6 py-4 fixed w-full top-0 z-50 rounded-lg transition-all duration-300 ${
+        className={`m-0 px-4 py-4 fixed w-full top-0 z-50 rounded-lg transition-all duration-300 md:px-12 ${
           scrolled 
             ? 'bg-white/75 backdrop-blur-md dark:bg-gray-900/75 shadow-lg'
             : 'bg-white dark:bg-gray-900'
