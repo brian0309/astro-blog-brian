2a9943353c4890363e835d9dae7470c468e262f2
brian0309 <34000638+brian0309@users.noreply.github.com>
2025-07-22 22:38:53 +0800

    docs: update site description to include personal introduction
    The site description was updated to provide a more personalized introduction rather than a generic welcome message.

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Brian Carlo ';
-export const SITE_DESCRIPTION = 'Welcome to site!';
+export const SITE_DESCRIPTION = "Hi I'm Brian Carlo, your versatile techy freelancer!";
