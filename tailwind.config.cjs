c7275cfb3f17c7eee6ea5e71cc9a185300731f5c
brian0309 <34000638+brian0309@users.noreply.github.com>
2025-07-22 22:18:46 +0800

    Update index

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,16 @@ module.exports = {
   darkMode: 'class',
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#2c3e50',
+        lightGray: '#999',
+        softGray: '#666',
+      },
+      backgroundImage: {
+        'blue-pink-gradient': 'linear-gradient(to right, #3b82f6, #ec4899)',
+      },
+    },
   },
   plugins: [],
 };
