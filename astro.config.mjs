baee47ab3b69c56fe287e5c59c6cac8b40094b10
brian0309 <34000638+brian0309@users.noreply.github.com>
2025-07-23 11:00:29 +0800

    Update site config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://briancarlo.pages.dev',
   integrations: [mdx(), sitemap(), react(), tailwind()],
 });
